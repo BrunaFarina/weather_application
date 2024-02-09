3e44187345c0a55dd24984233ee91e01109f8110
Bruna Farina <82400814+BrunaFarina@users.noreply.github.com>
2024-02-09 15:25:49 +0100

    Add files via upload

---
+++ weather_app.py
@@ -0,0 +1,18 @@
+import requests, json
+
+api_key = "insert_your_API_key_here"
+city_name = ""
+def get_weather(city_name, api_key):
+    print("Welcome to Simple Weather App!")
+    city_name = input("Enter city name: ")
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    data = response.json()
+    if response.status_code == 200:
+        print(f"Weather in {city_name}:")
+        print(f"- Temperature: {data['main']['temp']} °C")
+        print(f"- Condition: {data['weather'][0]['main']}")
+    else:
+        print("No data available")
+
+get_weather(city_name, api_key)
